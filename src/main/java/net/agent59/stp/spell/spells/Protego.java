e2b27ef1f27a065c36e3279121ca480eb6b48d0f
Agent59 <agent59@ripakewitz.net>
2022-05-27 00:41:16 +0200

    fixed protego colliding issues

--- src/main/java/net/agent59/stp/spell/spells/Protego.java
+++ src/main/java/net/agent59/stp/spell/spells/Protego.java
@@ -2,20 +2,26 @@ package net.agent59.stp.spell.spells;
 
 import net.agent59.stp.entity.ModEntities;
 import net.agent59.stp.entity.custom.RayEntity;
+import net.agent59.stp.item.ModItems;
 import net.agent59.stp.spell.SpellInterface;
+import net.agent59.stp.spell.SpellType;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Box;
+
+import java.util.List;
+import java.util.Objects;
 
 
 public class Protego extends Item implements SpellInterface {
     private static final String NAME = "Protego";
     private static final int RANGE = 2;
     private static final int CASTING_COOLDOWN = 25;
-    private static final int MAX_LIVETIME = 30;
+    private static final SpellType SPELLTYPE = SpellType.CHARM;
+    private static final int MAX_LIVETIME = 100;
 
     public Protego(Settings settings) {
         super(settings);
@@ -37,31 +43,35 @@ public class Protego extends Item implements SpellInterface {
     }
 
     @Override
+    public SpellType getSpellType() {
+        return SPELLTYPE;
+    }
+
+    @Override
     public void execute(ServerPlayerEntity player) {
         ServerWorld world = player.getWorld();
 
         RayEntity protegoWall = new RayEntity(ModEntities.PROTEGO_WALL, world) {
-            private int age = 0;
-
             @Override
-            protected void onEntityHit(EntityHitResult entityHitResult) {
-                Entity entity = entityHitResult.getEntity();
-                if (entity instanceof RayEntity) {
-                    entity.kill();
-                    this.kill();
-                }
-            }
+            public void tick2() {
+                Box box = this.getBoundingBox();
+                List<Entity> list = this.world.getOtherEntities(null, box); // TODO test the size of the box in multiplayer
 
-            @Override
-            public void tick() {
-                super.tick();
-                ++this.age;
-                if (this.age >= MAX_LIVETIME) {
-                    this.kill();
+                for (Entity entity: list) {
+                    if (entity instanceof RayEntity) {
+                        if (Objects.equals(((RayEntity) entity).getSpellName(), ((Stupefy)ModItems.STUPEFY).getStringName())) {
+                            entity.kill();
+                            this.kill();
+                            break;
+                        }
+                    }
                 }
             }
         };
         protegoWall.setOwner(player);
+        protegoWall.setMaxLifetime(MAX_LIVETIME);
+        protegoWall.setSpellType(SPELLTYPE);
+        protegoWall.setSpellName(NAME);
         HitResult hitResult = player.raycast(RANGE, 0, false);
         protegoWall.setPosition(hitResult.getPos().getX(), hitResult.getPos().getY() -1, hitResult.getPos().getZ());
         protegoWall.setYaw(player.getYaw());
