2df9d0d1752da4369601dd0299f7452d112e2acb
Agent59 <agent59@ripakewitz.net>
2024-09-22 00:18:05 +0200

    removed old spells and added spell-icons
    Signed-off-by: Agent59 <agent59@ripakewitz.net>

--- src/main/java/net/agent59/item/ModItems.java
+++ src/main/java/net/agent59/item/ModItems.java
@@ -3,7 +3,6 @@ package net.agent59.item;
 import net.agent59.Main;
 import net.agent59.item.custom.GuideBookItem;
 import net.agent59.item.custom.WandItem;
-import net.agent59.spell.spells.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -29,9 +28,7 @@ public class ModItems {
 
     private static Item registerItem(String name, Item item, @Nullable RegistryKey<ItemGroup> group) {
         Item item1 = Registry.register(Registries.ITEM, new Identifier(Main.MOD_ID, name), item);
-        if (group != null) {
-            ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item1));
-        }
+        if (group != null) ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item1));
         return item1;
     }
 
@@ -39,68 +36,16 @@ public class ModItems {
         return registerItem(name, item, null);
     }
 
-    // create a SpellItem for every spell: needed for the cooldown-system
-    public static final Item AGUAMENTI = registerItem("aguamenti",
-            new Aguamenti(new FabricItemSettings()));
+    /**
+     * Registers an item whose purpose is only to be used for displaying an icon for a spell.
+     */
+    public static Item registerDisplayIconItem(Identifier id) {
+        return Registry.register(Registries.ITEM, id, new Item(new FabricItemSettings()));
+    }
 
-    public static final Item STUPEFY = registerItem("stupefy",
-            new Stupefy(new FabricItemSettings()));
-
-    public static final Item PROTEGO = registerItem("protego",
-            new Protego(new FabricItemSettings()));
-
-    public static final Item HOMENUM_REVELIO = registerItem("homenum_revelio",
-            new HomenumRevelio(new FabricItemSettings()));
-
-    public static final Item APPARATE = registerItem("apparate",
-            new Apparate(new FabricItemSettings()));
-
-    public static final Item ASCENDIO = registerItem("ascendio",
-            new Ascendio(new FabricItemSettings()));
-
-    public static final Item EXPELLIARMUS = registerItem("expelliarmus",
-            new Expelliarmus(new FabricItemSettings()));
-
-    public static final Item PETRIFICUS_TOTALUS = registerItem("petrificus_totalus",
-            new PetrificusTotalus(new FabricItemSettings()));
-
-    public static final Item ACCIO = registerItem("accio",
-            new Accio(new FabricItemSettings()));
-
-    public static final Item ALARTE_ASCENDARE = registerItem("alarte_ascendare",
-            new AlarteAscendare(new FabricItemSettings()));
-
-    public static final Item MELOFORS = registerItem("melofors",
-            new Melofors(new FabricItemSettings()));
-
-    public static final Item FUMOS = registerItem("fumos",
-            new Fumos(new FabricItemSettings()));
-
-    public static final Item INCENDIO = registerItem("incendio",
-            new Incendio(new FabricItemSettings()));
-
-    public static final Item HERBIVICUS = registerItem("herbivicus",
-            new Herbivicus(new FabricItemSettings()));
-
-    public static final Item CISTEM_APERIO = registerItem("cistem_aperio",
-            new CistemAperio(new FabricItemSettings()));
-
-    public static final Item FLIPENDO = registerItem("flipendo",
-            new Flipendo(new FabricItemSettings()));
-
-    public static final Item LUMOS = registerItem("lumos",
-            new Lumos(new FabricItemSettings()));
-
-    public static final Item NOX = registerItem("nox",
-            new Nox(new FabricItemSettings()));
-
-    public static final Item TEMPEST = registerItem("tempest",
-            new Tempest(new FabricItemSettings()));
-
-    public static final Item PORTUS = registerItem("portus",
-            new Portus(new FabricItemSettings()));
-
-
+    private static Item registerDisplayIconItem(String path) {
+        return registerDisplayIconItem(Main.id(path));
+    }
 
     public static void registerModItems() {
         Main.LOGGER.info("Registering Mod Items for " + Main.MOD_ID);
