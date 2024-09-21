ffd9a197927e2ac77c74d1ac5f902a0941333d09
Agent59 <agent59@ripakewitz.net>
2024-09-22 00:10:38 +0200

    renamed and fixed imports
    Signed-off-by: Agent59 <agent59@ripakewitz.net>

--- src/client/java/net/agent59/StSClient.java
+++ src/client/java/net/agent59/StSClient.java
@@ -2,20 +2,19 @@ package net.agent59;
 
 import net.agent59.gui.ModScreens;
 import net.agent59.render.ModEntityRenderer;
-import net.agent59.render.block.ModBlockEntityRenderer;
+import net.agent59.render.block.StSBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class Client implements ClientModInitializer {
+public class StSClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         CustomKeybindings.registerCustomKeybindings();
         ModEntityRenderer.registerModEntityRenderers();
-        ModBlockEntityRenderer.registerModBlockEntityRenderers();
-        ClientNetwork.registerNetworkPackets();
+        StSBlockEntityRenderer.registerModBlockEntityRenderers();
         StSClientNetwork.registerNetworkPackets();
         ModScreens.registerModScreens();
     }
