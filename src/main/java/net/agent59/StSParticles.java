c91064b39e689fdb70fa52acc14da72175bf5d51
Agent59 <agent59@ripakewitz.net>
2024-09-21 15:04:32 +0200

    renamed and now uses Main.id(path)
    Signed-off-by: Agent59 <agent59@ripakewitz.net>

--- src/main/java/net/agent59/StSParticles.java
+++ src/main/java/net/agent59/StSParticles.java
@@ -4,16 +4,15 @@ import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 
-public class ModParticles {
+public class StSParticles {
 
     public static final DefaultParticleType EMPTY_PARTICLE = FabricParticleTypes.simple();
 
     public static void registerModParticles() {
-        Main.LOGGER.info("Registering Mod Particles for " + Main.MOD_ID);
+        Main.LOGGER.info("Registering Mod Particles for " + Main.MOD_NAME);
 
-        Registry.register(Registries.PARTICLE_TYPE, new Identifier(Main.MOD_ID, "empty"), EMPTY_PARTICLE);
+        Registry.register(Registries.PARTICLE_TYPE, Main.id("empty"), EMPTY_PARTICLE);
     }
 
 }
